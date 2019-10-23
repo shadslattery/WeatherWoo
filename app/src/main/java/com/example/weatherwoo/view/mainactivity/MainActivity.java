22121e4c8c0c7b18db8f7c6d285e0c4b30a3748c
shadslattery <shadslattery@gmail.com>
2019-10-23 19:55:11 -0400

    This is now displaying the Text and the LATITUDE AND LONGITUDE

--- app/src/main/java/com/example/weatherwoo/view/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/weatherwoo/view/mainactivity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.weatherwoo.view.mainactivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -9,7 +11,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.weatherwoo.R;
+import com.example.weatherwoo.model.Currently;
+import com.example.weatherwoo.model.Daily;
+import com.example.weatherwoo.model.Hourly;
+import com.example.weatherwoo.model.WeatherResponse;
 import com.example.weatherwoo.view.NameViewModel;
+import com.example.weatherwoo.view.adapter.dailyadapter.DailyAdapter;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         // Initializing variables
         viewModel = ViewModelProviders.of(this).get(NameViewModel.class);
         rvHourly = findViewById(R.id.rv_hourly);
-        setupHourlyRecyclerView;
+        setupHourlyRecyclerView();
         rvDaily = findViewById(R.id.rv_daily);
         setupDailyRecyclerView();
 
@@ -41,8 +52,48 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupDailyRecyclerView() {
         rvDaily.setLayoutManager(new LinearLayoutManager(this));
-            rvDaily.setHasFixedSize(true);
-            rvDaily.addItemDecoration(new DividerItemDecoration(this,
-        DividerItemDecoration.VERTICAL));
+        rvDaily.setHasFixedSize(true);
+        rvDaily.addItemDecoration(new DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL));
+    }
+
+    private void LoadWeather() {
+        viewModel.getWeatherCall(LATITUDE, LONGITUDE).enqueue(new Callback<WeatherResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<WeatherResponse> call, @NonNull Response<WeatherResponse> response) {
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+                WeatherResponse weatherResponse = response.body();
+                if (weatherResponse != null) {
+                    // Load Currently
+                    // Todo:
+
+                    //Load Hourly
+                    // Todo:
+
+                    // Load Daily
+                    LoadDaily(weatherResponse.getDaily());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<WeatherResponse> call, @NonNull Throwable t) {
+                Toast.makeText(MainActivity.this, "FAILURE", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void LoadCurrently(Currently currently) {
+
+        // Todo: Pass data into views
+    }
+
+    private void LoadHourly(Hourly hourly) {
+
+    }
+
+    private void LoadDaily(Daily daily) {
+        DailyAdapter adapter = new DailyAdapter(daily.getData());
+        rvDaily.setAdapter(adapter);
     }
 }
